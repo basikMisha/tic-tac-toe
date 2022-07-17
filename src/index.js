b94db03bfe40d0b76b0b35ec077afe51552704a9
basikMisha <michail.rastigaev@gmail.com>
2022-07-18 02:52:11 +0300

    tic-tac-toe v.1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import Square from "./components/Square";
+import './index.css'
+import { calcWinner } from "./func/calcWinner";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const initialState = {
+  squares: Array(9).fill(null),
+  xIsnext: true,
+}
+  
+  class Board extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+    }
+    
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        
+        if(calcWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsnext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsnext: !this.state.xIsnext,
+        })
+        this.restart = this.restart.bind(this)
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    renderSquare(i) {
+      return <Square value = {this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+      />;
+    }
+
+    restart() {
+      
+      this.setState(initialState)
+    }
+  
+    render() {
+
+      const winner = calcWinner(this.state.squares);
+      let status;
+
+      if(winner) {
+        status = `Winner is ${winner}`;
+      } else {
+        status =  'Next player: ' + (this.state.xIsnext ? 'X' : 'O');
+      }
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <button
+           style={{marginTop: 20}}
+           onClick = { this.restart}
+           >Restart</button>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
+  
